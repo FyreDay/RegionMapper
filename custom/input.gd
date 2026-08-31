3bec0a77528d6c22b966a1f6d01195968ad673b9
FyreDay <fyredaymc@gmail.com>
2026-08-31 13:55:40 -0500

    move to on input

--- custom/input.gd
+++ custom/input.gd
@@ -1,6 +1,49 @@
 class_name CustomEdit
 extends LineEdit
 
+var _web_paste_callback
+
+func _ready() -> void:
+    if OS.get_name() == "Web":
+        _setup_web_paste_bridge()
 
 func _gui_input(event: InputEvent) -> void:
-    print(event.as_text())
+    if OS.get_name() == "Web" and event is InputEventKey and event.pressed and not event.echo:
+        if event.keycode == KEY_V and (event.ctrl_pressed or event.meta_pressed):
+            print("Ctrl v pressed")
+            print(event.as_text())
+            get_viewport().set_input_as_handled()  
+    
+func _setup_web_paste_bridge() -> void:
+    _web_paste_callback = JavaScriptBridge.create_callback(_on_web_paste)
+    var window = JavaScriptBridge.get_interface("window")
+    window.godotPasteCallback = _web_paste_callback
+    JavaScriptBridge.eval("""
+    (function() {
+        window.addEventListener('paste', function(e) {
+            console.log('[paste] event fired, target=', e.target);
+            var cd = e.clipboardData || window.clipboardData;
+            if (!cd) { console.log('[paste] no clipboardData object'); return; }
+            var text = cd.getData('text/plain');
+            console.log('[paste] captured text:', JSON.stringify(text));
+            if (window.godotPasteCallback) {
+                window.godotPasteCallback(text);
+            } else {
+                console.log('[paste] godotPasteCallback missing!');
+            }
+        });
+        console.log('[paste] listener registered');
+    })();
+    """, true)
+
+func _on_web_paste(args: Array) -> void:
+    print("[paste] _on_web_paste called with: ", args)
+    if args.is_empty():
+        return
+    var text := str(args[0])
+    var focused := get_viewport().gui_get_focus_owner()
+    print("[paste] focused node: ", focused)
+    if focused == self:
+        DisplayServer.clipboard_set(text)
+        menu_option(LineEdit.MENU_PASTE)
+        print("[paste] pasted into LineEdit")
--- main.gd
+++ main.gd
@@ -16,8 +16,6 @@ var _web_png_upload_callback
 
 var undo_redo := UndoRedo.new()
 
-var _web_paste_callback
-
 func _ready() -> void:
     ui.region_scale_changed.connect(map._on_region_scale)
     node_manager.setup(undo_redo)
@@ -37,8 +35,7 @@ func _ready() -> void:
     ui.flags_updated.connect(node_manager._on_flags_updated)
     node_manager.load_flag_names.connect(ui._on_flags_updated)
     
-    if OS.get_name() == "Web":
-        _setup_web_paste_bridge()
+    
 
 func _process(_delta: float) -> void:
     pass
@@ -53,12 +50,7 @@ func _input(event: InputEvent) -> void:
         get_viewport().set_input_as_handled()
         return
     
-    if OS.get_name() == "Web" and event is InputEventKey and event.pressed and not event.echo:
-        if event.keycode == KEY_V and (event.ctrl_pressed or event.meta_pressed):
-            print("Ctrl v pressed")
-            if get_viewport().gui_get_focus_owner() is LineEdit:
-                print("Found Line Edit")
-                get_viewport().set_input_as_handled()
+   
     
 func _on_load_data(path):
     var zip := ZIPReader.new()
@@ -386,37 +378,3 @@ func _on_web_png_uploaded(args: Array) -> void:
 
     map.set_map(image, file_name, 10)
     
-
-func _setup_web_paste_bridge() -> void:
-    _web_paste_callback = JavaScriptBridge.create_callback(_on_web_paste)
-    var window = JavaScriptBridge.get_interface("window")
-    window.godotPasteCallback = _web_paste_callback
-    JavaScriptBridge.eval("""
-    (function() {
-        window.addEventListener('paste', function(e) {
-            console.log('[paste] event fired, target=', e.target);
-            var cd = e.clipboardData || window.clipboardData;
-            if (!cd) { console.log('[paste] no clipboardData object'); return; }
-            var text = cd.getData('text/plain');
-            console.log('[paste] captured text:', JSON.stringify(text));
-            if (window.godotPasteCallback) {
-                window.godotPasteCallback(text);
-            } else {
-                console.log('[paste] godotPasteCallback missing!');
-            }
-        });
-        console.log('[paste] listener registered');
-    })();
-    """, true)
-
-func _on_web_paste(args: Array) -> void:
-    print("[paste] _on_web_paste called with: ", args)
-    if args.is_empty():
-        return
-    var text := str(args[0])
-    var focused := get_viewport().gui_get_focus_owner()
-    print("[paste] focused node: ", focused)
-    if focused is LineEdit:
-        DisplayServer.clipboard_set(text)
-        focused.menu_option(LineEdit.MENU_PASTE)
-        print("[paste] pasted into LineEdit")
